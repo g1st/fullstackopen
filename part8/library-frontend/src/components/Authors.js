ab79af2596638d692aec071f92eb5a40ff1b0a33
g1st <gintstan@gmail.com>
2020-02-08 13:48:28 +0000

    Ex.  8.8

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+const Authors = props => {
+  const { loading, error, data } = useQuery(ALL_AUTHORS);
+  if (!props.show) {
+    return null;
+  }
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error :(</div>;
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {data.allAuthors.map(a => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
---
+++ part8/library-frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import App from './App';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
+
+const ApolloApp = AppComponent => (
+  <ApolloProvider client={client}>
+    <AppComponent />
+  </ApolloProvider>
+);
+
+ReactDOM.render(ApolloApp(App), document.getElementById('root'));
