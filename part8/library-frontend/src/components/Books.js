f6a1061e5e1071151803393329bcccb10dfeb0d6
g1st <gintstan@gmail.com>
2020-02-08 13:55:46 +0000

    Ex. 8.9

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-const Books = (props) => {
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+    }
+  }
+`;
+
+const Books = props => {
+  const { loading, error, data } = useQuery(ALL_BOOKS);
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error :(</div>;
 
   return (
     <div>
@@ -15,24 +29,20 @@ const Books = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th>author</th>
+            <th>published</th>
           </tr>
-          {books.map(a =>
+          {data.allBooks.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
               <td>{a.published}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
