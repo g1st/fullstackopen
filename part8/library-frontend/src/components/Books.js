8fc5fcbb97852d0f3ade8fd7d66dafd60ce9bff6
g1st <gintstan@gmail.com>
2020-02-10 20:00:11 +0000

    Ex. 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 
@@ -8,6 +8,7 @@ export const ALL_BOOKS = gql`
       title
       published
       id
+      genres
       author {
         name
       }
@@ -17,16 +18,44 @@ export const ALL_BOOKS = gql`
 
 const Books = props => {
   const { loading, error, data } = useQuery(ALL_BOOKS);
+  const [genre, setGenre] = useState('all genres');
+
   if (!props.show) {
     return null;
   }
+
+  const extractGenres = data => {
+    const genres = data.allBooks.reduce(
+      (acc, book) => {
+        for (const genre of book.genres) {
+          if (!acc.includes(genre)) {
+            acc.push(genre);
+          }
+        }
+        return acc;
+      },
+      ['all genres']
+    );
+    return genres;
+  };
+
+  const genreButtons = extractGenres(data).map(genre => (
+    <button key={genre} onClick={() => setGenre(genre)}>
+      {genre}
+    </button>
+  ));
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error :(</div>;
 
   return (
     <div>
+      {extractGenres(data)}
       <h2>books</h2>
+
+      <div>
+        in genre <strong>{genre}</strong>
+      </div>
 
       <table>
         <tbody>
@@ -35,15 +64,22 @@ const Books = props => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {data.allBooks.map(a => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published === 0 ? '' : a.published}</td>
-            </tr>
-          ))}
+          {data.allBooks.map(book => {
+            if (book.genres.includes(genre) || genre === 'all genres') {
+              return (
+                <tr key={book.title}>
+                  <td>{book.title}</td>
+                  <td>{book.author.name}</td>
+                  <td>{book.published === 0 ? '' : book.published}</td>
+                </tr>
+              );
+            }
+            return null;
+          })}
         </tbody>
       </table>
+      <br />
+      <div>{genreButtons}</div>
     </div>
   );
 };
