dd0f7e959382eedecabdddb5aab78b8f965b61b6
g1st <gintstan@gmail.com>
2020-04-13 17:09:24 +0100

    Ex. 9.4

--- part9/index.ts
+++ part9/index.ts
@@ -1,25 +1,13 @@
-type Operation = 'multiply' | 'add' | 'divide';
+import express from 'express';
 
-type Result = number;
+const PORT = 3000;
 
-const calculator = (a: number, b: number, op: Operation): Result => {
-  switch (op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-      if (b === 0) throw new Error("Can't divide by 0!");
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
-  }
-};
+const app = express();
 
-try {
-  console.log(calculator(1, 5, 'divide'));
-} catch (e) {
-  console.log('Something went wrong, error message: ', e.message);
-}
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
 
-console.log(process.argv);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
